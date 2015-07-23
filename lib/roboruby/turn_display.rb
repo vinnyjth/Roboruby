eb884f462133d64d0f787147533c7a2aca617946
Vincent Wilson <vincent.movieman@gmail.com>
2015-07-22 20:48:57 -0400

    Made my mapper work

--- lib/roboruby/turn_display.rb
+++ lib/roboruby/turn_display.rb
@@ -5,15 +5,6 @@ module Roboruby
     #Will eventully become Roboruby::Display
 
     def self.render_erb(turns, output, template="turn")
-      robo_turns = turns
-      turns = robo_turns.map(&:arena_map)
-      bots = robo_turns.map(&:bots)
-      turns = turns.each_with_index.map do |turn, i|
-        bots[i].each_with_index do |b|
-          turn[b.position.y][b.position.x] = -1
-        end
-        turn
-      end
       @turns = turns
       erb = ERB.new(File.open("#{__dir__}/templates/#{template}.html.erb").read, 0, '>')
       File.open("#{__dir__}/#{output}.html", 'w') { |f| f.write erb.result binding }
--- test.rb
+++ test.rb
@@ -4,8 +4,14 @@ m = Roboruby::Match.new a
 b0 = Roboruby::Bot.new("basic", 40, m)
 b1 = Roboruby::Bot.new("basic2", 40, m)
 b1.position.position = {y: 1, x: 4}
-@turns = []
+turns_data = []
 20.times do
-    @turns << Roboruby::Turn.new(m)
+    turn = Roboruby::Turn.new(m)
+    arena_map = turn.arena_map.deep_dup
+    bots = turn.bots
+    bots.each_with_index do |b|
+      arena_map[b.position.y][b.position.x] = -1
+    end
+    turns_data << arena_map
 end
-Roboruby::TurnDisplay.render_erb(@turns, "bakbal")
+Roboruby::TurnDisplay.render_erb(turns_data, "bakbal")
